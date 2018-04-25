7aa383220e83a3d236fa20634c201dd014bb13a7
Elio Struyf <info@estruyf.be>
2018-04-25 20:28:24 +0200

    Various fixes for the list picker

--- src/controls/listPicker/ListPicker.tsx
+++ src/controls/listPicker/ListPicker.tsx
@@ -1,142 +1,164 @@
 import * as React from 'react';
 import { IDropdownOption, IDropdownProps, Dropdown } from 'office-ui-fabric-react/lib/components/Dropdown';
 import { Spinner, SpinnerSize } from 'office-ui-fabric-react/lib/components/Spinner';
-
 import { IListPickerProps, IListPickerState } from './IListPicker';
 import { ISPService } from '../../services/ISPService';
 import { SPServiceFactory } from '../../services/SPServiceFactory';
+import * as appInsights from '../../common/appInsights';
 
 import styles from './ListPicker.module.scss';
 
 /**
- * Empty list value, to be checked for single list selection
- */
+* Empty list value, to be checked for single list selection
+*/
 const EMPTY_LIST_KEY = 'NO_LIST_SELECTED';
 
 /**
- * Renders the controls for the ListPicker component
- */
+* Renders the controls for the ListPicker component
+*/
 export class ListPicker extends React.Component<IListPickerProps, IListPickerState> {
-    private _options: IDropdownOption[] = [];
-    private _selectedList: string | string[];
-    
-    /**
-     * Constructor method
-     */
-    constructor(props: IListPickerProps) {
-        super(props);
-        
-        console.debug('selectedList', this.props.selectedList);
+  private _options: IDropdownOption[] = [];
+  private _selectedList: string | string[];
 
-        this.state = {
-            options: this._options,
-            loading: false
-        };
+  /**
+  * Constructor method
+  */
+  constructor(props: IListPickerProps) {
+    super(props);
 
-        this.onChanged = this.onChanged.bind(this);
+    appInsights.track('ReactListPicker');
+
+    this.state = {
+      options: this._options,
+      loading: false
+    };
+
+    this.onChanged = this.onChanged.bind(this);
+  }
+
+  /**
+  * Lifecycle hook when component is mounted
+  */
+  public componentDidMount() {
+    this.loadLists();
+  }
+
+  /**
+   * componentDidUpdate lifecycle hook
+   * @param prevProps
+   * @param prevState
+   */
+  public componentDidUpdate(prevProps: IListPickerProps, prevState: IListPickerState): void {
+    if (
+      prevProps.baseTemplate !== this.props.baseTemplate ||
+      prevProps.includeHidden !== this.props.includeHidden ||
+      prevProps.orderBy !== this.props.orderBy ||
+      prevProps.selectedList !== this.props.selectedList
+    ) {
+      this.loadLists();
     }
+  }
 
-    /**
-     * Lifecycle hook when component is mounted
-     */
-    public componentDidMount() {
-        this.loadLists();
-    }
+  /**
+  * Loads the list from SharePoint current web site
+  */
+  private loadLists() {
+    const { context, baseTemplate, includeHidden, orderBy, multiSelect, selectedList } = this.props;
 
-    /**
-     * Loads the list from SharePoint current web site
-     */
-    private loadLists() {
-        const { context, baseTemplate, includeHidden, orderBy, multiSelect, selectedList } = this.props;
+    // Show the loading indicator and disable the dropdown
+    this.setState({ loading: true });
 
-        // Show the loading indicator and disable the dropdown
-        this.setState({ loading: true });
-
-        const service: ISPService = SPServiceFactory.createService(context, true, 5000);
-        service.getLibs({ 
-            baseTemplate: baseTemplate,
-            includeHidden: includeHidden,
-            orderBy: orderBy
-        }).then((results) => {
-            // Start mapping the lists to the dropdown option
-            results.value.map(list => {                
-                this._options.push({
-                    key: list.Id,
-                    text: list.Title
-                });
-            });
-
-            if (multiSelect !== true) {
-                // Add option to unselct list
-                this._options.unshift({
-                    key: EMPTY_LIST_KEY,
-                    text: ''
-                });
-            }
-
-            this._selectedList = this.props.selectedList;
-
-            // Hide the loading indicator and set the dropdown options and enable the dropdown
-            this.setState({
-                loading: false,
-                options: this._options,
-                selectedList: this._selectedList
-            });
+    const service: ISPService = SPServiceFactory.createService(context, true, 5000);
+    service.getLibs({
+      baseTemplate: baseTemplate,
+      includeHidden: includeHidden,
+      orderBy: orderBy
+    }).then((results) => {
+      // Start mapping the lists to the dropdown option
+      results.value.map(list => {
+        this._options.push({
+          key: list.Id,
+          text: list.Title
         });
+      });
+
+      if (multiSelect !== true) {
+        // Add option to unselct list
+        this._options.unshift({
+          key: EMPTY_LIST_KEY,
+          text: ''
+        });
+      }
+
+      this._selectedList = this.props.selectedList;
+
+      // Hide the loading indicator and set the dropdown options and enable the dropdown
+      this.setState({
+        loading: false,
+        options: this._options,
+        selectedList: this._selectedList
+      });
+    });
+  }
+
+  /**
+  * Raises when a list has been selected
+  * @param option the new selection
+  * @param index the index of the selection
+  */
+  private onChanged(option: IDropdownOption, index?: number): void {
+    const { multiSelect, onSelectionChanged } = this.props;
+
+    if (multiSelect === true) {
+      if (!this._selectedList) {
+        this._selectedList = [] as string[];
+      }
+
+      const selectedLists: string[] = this._selectedList as string[];
+      // Check if option was selected
+      if (option.selected) {
+        selectedLists.push(option.key as string);
+      } else {
+        // Filter out the unselected list
+        this._selectedList = selectedLists.filter(list => list !== option.key);
+      }
+    } else {
+      this._selectedList = option.key as string;
     }
 
-    /**
-     * Raises when a list has been selected
-     * @param option the new selection
-     * @param index the index of the selection
-     */
-    private onChanged(option: IDropdownOption, index?: number): void {
-        const { multiSelect, onSelectionChanged } = this.props;
-        
-        if (multiSelect === true) {
-            if (this._selectedList === undefined) {
-                this._selectedList = new Array<string>();
-            }
-            (this._selectedList as string[]).push(option.key as string);
-        } else {
-            this._selectedList = option.key as string;
-        }
+    if (onSelectionChanged) {
+      onSelectionChanged(this._selectedList);
+    }
+  }
 
-        if (onSelectionChanged) {
-            onSelectionChanged(this._selectedList);
-        }
+  /**
+  * Renders the ListPicker controls with Office UI Fabric
+  */
+  public render(): JSX.Element {
+    const { loading, options, selectedList } = this.state;
+    const { className, disabled, multiSelect, label, placeHolder } = this.props;
+
+    const dropdownOptions: IDropdownProps = {
+      className: className,
+      options: options,
+      disabled: ( loading || disabled ),
+      label: label,
+      placeHolder: placeHolder,
+      onChanged: this.onChanged
+    };
+
+    if (multiSelect === true) {
+      dropdownOptions.multiSelect = true;
+      dropdownOptions.selectedKeys = selectedList as string[];
+    } else {
+      dropdownOptions.selectedKey = selectedList as string;
     }
 
-    /**
-     * Renders the ListPicker controls with Office UI Fabric
-     */
-    public render(): JSX.Element {
-        const { loading, options, selectedList } = this.state;
-        const { className, disabled, multiSelect, label, placeHolder } = this.props;
-
-        const dropdownOptions: IDropdownProps = {
-            className: className,
-            options: options,
-            disabled: ( loading || disabled ),
-            label: label,
-            placeHolder: placeHolder,
-            onChanged: this.onChanged
-        };
-
-        if (multiSelect === true) {
-            dropdownOptions.multiSelect = true;
-            dropdownOptions.selectedKeys = selectedList as string[];
-        } else {
-            dropdownOptions.selectedKey = selectedList as string;
-        }
-
-        return (
-            <div className={ styles.listPicker }>
-                { loading && <Spinner className={ styles.spinner } size={SpinnerSize.xSmall} /> }
-                <Dropdown {...dropdownOptions} />
-            </div>
-        );
-    }
+    return (
+      <div className={ styles.listPicker }>
+        { loading && <Spinner className={ styles.spinner } size={SpinnerSize.xSmall} /> }
+        <Dropdown {...dropdownOptions} />
+      </div>
+    );
+  }
 }
-
-export default ListPicker;
--- src/services/SPService.ts
+++ src/services/SPService.ts
@@ -1,32 +1,36 @@
 import { ISPService, ILibsOptions, LibsOrderBy } from "./ISPService";
 import { ISPLists } from "../common/SPEntities";
-import { IWebPartContext } from "@microsoft/sp-webpart-base";
+import { WebPartContext } from "@microsoft/sp-webpart-base";
+import { ApplicationCustomizerContext } from '@microsoft/sp-application-base';
 import { SPHttpClient, SPHttpClientResponse } from "@microsoft/sp-http";
 
-export class SPService implements ISPService {
-    private readonly _context: IWebPartContext;
-    constructor(context: IWebPartContext) {
-        this._context = context;
-    }
-    public getLibs(options?: ILibsOptions): Promise<ISPLists> {
-        let filtered: boolean;
-        let queryUrl: string = `${this._context.pageContext.web.absoluteUrl}/_api/web/lists?$select=Title,id,BaseTemplate`;
-        if (options.orderBy !== null) {
-            queryUrl += `&$orderby=${options.orderBy === LibsOrderBy.Id ? 'Id': 'Title'}`;
-        }
-        if (options.baseTemplate !== null) {
-            queryUrl += `&$filter=BaseTemplate eq ${options.baseTemplate}`;
-            filtered = true;
-        }
-        if (options.includeHidden === false) {
-            queryUrl += filtered ? ' and Hidden eq false' : '&$filter=Hidden eq false';
-            filtered = true;
-        }
-        return this._context.spHttpClient.get(queryUrl, SPHttpClient.configurations.v1)
-            .then((response: SPHttpClientResponse) => {
-                return response.json();
-            }) as Promise<ISPLists>;
-    }
-}
+export default class SPService implements ISPService {
 
-export default SPService;
+  constructor(private _context: WebPartContext | ApplicationCustomizerContext) {}
+
+  /**
+   * Get lists or libraries
+   * @param options
+   */
+  public getLibs(options?: ILibsOptions): Promise<ISPLists> {
+    let filtered: boolean;
+    let queryUrl: string = `${this._context.pageContext.web.absoluteUrl}/_api/web/lists?$select=Title,id,BaseTemplate`;
+
+    if (options.orderBy) {
+      queryUrl += `&$orderby=${options.orderBy === LibsOrderBy.Id ? 'Id': 'Title'}`;
+    }
+
+    if (options.baseTemplate) {
+      queryUrl += `&$filter=BaseTemplate eq ${options.baseTemplate}`;
+      filtered = true;
+    }
+
+    if (options.includeHidden === false) {
+      queryUrl += filtered ? ' and Hidden eq false' : '&$filter=Hidden eq false';
+      filtered = true;
+    }
+
+    return this._context.spHttpClient.get(queryUrl, SPHttpClient.configurations.v1)
+    .then(response => response.json()) as Promise<ISPLists>;
+  }
+}
