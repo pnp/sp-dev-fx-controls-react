c1bbd2946a0564d292491d64ba60a1269440905c
Markus Möller <markus.moeller.ed@outlook.de>
2018-09-23 09:36:43 +0200

    Create de-de.ts

---
+++ src/loc/de-de.ts
@@ -0,0 +1,43 @@
+declare var define: any;
+
+define([], () => {
+  return {
+    'SiteBreadcrumbLabel': 'Website breadcrumb',
+
+    'ListViewGroupEmptyLabel': 'Empty',
+
+    'WebPartTitlePlaceholder': 'Web part Titel',
+    'WebPartTitleLabel': 'Titel hinzufügen',
+
+    "DateTime": {
+      "L_RelativeDateTime_AFewSecondsFuture": "In wenigen Sekunden",
+      "L_RelativeDateTime_AFewSeconds": "Vor wenigen Sekunden",
+      "L_RelativeDateTime_AboutAMinuteFuture": "In ungefähr einer Minute",
+      "L_RelativeDateTime_AboutAMinute": "Vor ungefähr einer Minute",
+      "L_RelativeDateTime_XMinutesFuture": "In {0} Minute||In {0} Minuten",
+      "L_RelativeDateTime_XMinutes": "Vor {0} Minute||Vor {0} Minuten",
+      "L_RelativeDateTime_XMinutesFutureIntervals": "1||2-",
+      "L_RelativeDateTime_XMinutesIntervals": "1||2-",
+      "L_RelativeDateTime_AboutAnHourFuture": "In about an hour",
+      "L_RelativeDateTime_AboutAnHour": "About an hour ago",
+      "L_RelativeDateTime_Tomorrow": "Tomorrow",
+      "L_RelativeDateTime_Yesterday": "Yesterday",
+      "L_RelativeDateTime_TomorrowAndTime": "Tomorrow at {0}",
+      "L_RelativeDateTime_YesterdayAndTime": "Yesterday at {0}",
+      "L_RelativeDateTime_XHoursFuture": "In {0} hour||In {0} hours",
+      "L_RelativeDateTime_XHours": "{0} hour ago||{0} hours ago",
+      "L_RelativeDateTime_XHoursFutureIntervals": "1||2-",
+      "L_RelativeDateTime_XHoursIntervals": "1||2-",
+      "L_RelativeDateTime_DayAndTime": "{0} at {1}",
+      "L_RelativeDateTime_XDaysFuture": "{0} day from now||{0} days from now",
+      "L_RelativeDateTime_XDays": "{0} day ago||{0} days ago",
+      "L_RelativeDateTime_XDaysFutureIntervals": "1||2-",
+      "L_RelativeDateTime_XDaysIntervals":  "1||2-",
+      "L_RelativeDateTime_Today": "Today"
+    },
+    "SendEmailTo": "Send an email to {0}",
+    "StartChatWith": "Start a chat with {0}",
+    "Contact": "Contact",
+    "UpdateProfile": "Update your profile"
+  };
+});
