e9c8692a85762f4aef6298b5c9ad808c137ce10c
João <joao.j.mendes@outlook.com>
2018-10-24 22:19:00 +0100

    Commit FieldPickerListData

--- src/controls/fields/fieldPickerListData/index.ts
+++ src/controls/fields/fieldPickerListData/index.ts
@@ -1,5 +1,5 @@
 // A file is required to be in the root of the /src directory by the TypeScript compiler
 export * from './IFieldPickerListDataProps';
 export * from './IFieldPickerListDataState';
-export * from '../services/SPservice';
+export * from '../../../services/SPService';
 export * from './FieldPickerListData';
