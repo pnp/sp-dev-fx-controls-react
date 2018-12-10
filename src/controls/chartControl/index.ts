1fa72bd0807d1c2430b760230258bb1fdf03e53b
Hugo Bernier <hugoabernier@live.ca>
2018-12-10 02:30:29 -0500

    Added color palettes, palette generator, and accessible chart tables

--- src/controls/chartControl/index.ts
+++ src/controls/chartControl/index.ts
@@ -1,2 +1,6 @@
 export * from './ChartControl.types';
 export * from './ChartControl';
+export * from './ChartColorPalettes';
+export * from './PaletteGenerator';
+export * from './AccessibleChartTable.types';
+export * from './AccessibleChartTable';
