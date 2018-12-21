100a876fafbd180113c6036788450805cb2ca884
Elio Struyf <info@estruyf.be>
2018-12-21 10:04:58 +0100

    #205 - Sort order fix in TermPicker

--- src/services/SPTermStorePickerService.ts
+++ src/services/SPTermStorePickerService.ts
@@ -57,7 +57,7 @@ export default class SPTermStorePickerService {
       return this.context.spHttpClient.post(this.clientServiceUrl, SPHttpClient.configurations.v1, httpPostOptions).then((serviceResponse: SPHttpClientResponse) => {
         return serviceResponse.json().then((serviceJSONResponse: any) => {
           // Construct results
-          let termStoreResult: ITermStore[] = serviceJSONResponse.filter(r => r['_ObjectType_'] === 'SP.Taxonomy.TermStore');
+          let termStoreResult: ITermStore[] = serviceJSONResponse.filter((r: { [x: string]: string; }) => r['_ObjectType_'] === 'SP.Taxonomy.TermStore');
           // Check if term store was retrieved
           if (termStoreResult.length > 0) {
             // Check if the termstore needs to be filtered or limited
@@ -146,13 +146,13 @@ export default class SPTermStorePickerService {
 
       return this.context.spHttpClient.post(this.clientServiceUrl, SPHttpClient.configurations.v1, httpPostOptions).then((serviceResponse: SPHttpClientResponse) => {
         return serviceResponse.json().then((serviceJSONResponse: any) => {
-          const termStoreResultTermSets: ITermSet[] = serviceJSONResponse.filter(r => r['_ObjectType_'] === 'SP.Taxonomy.TermSet');
+          const termStoreResultTermSets: ITermSet[] = serviceJSONResponse.filter((r: { [x: string]: string; }) => r['_ObjectType_'] === 'SP.Taxonomy.TermSet');
 
           if (termStoreResultTermSets.length > 0) {
             var termStoreResultTermSet = termStoreResultTermSets[0];
             termStoreResultTermSet.Terms = [];
             // Retrieve the term collection results
-            const termStoreResultTerms: ITerms[] = serviceJSONResponse.filter(r => r['_ObjectType_'] === 'SP.Taxonomy.TermCollection');
+            const termStoreResultTerms: ITerms[] = serviceJSONResponse.filter((r: { [x: string]: string; }) => r['_ObjectType_'] === 'SP.Taxonomy.TermCollection');
             if (termStoreResultTerms.length > 0) {
               // Retrieve all terms
               let terms = termStoreResultTerms[0]._Child_Items_;
@@ -264,7 +264,7 @@ export default class SPTermStorePickerService {
           return this.context.spHttpClient.post(this.clientServiceUrl, SPHttpClient.configurations.v1, httpPostOptions).then((serviceResponse: SPHttpClientResponse) => {
             return serviceResponse.json().then((serviceJSONResponse: any) => {
               // Retrieve the term collection results
-              const termStoreResult: ITerms[] = serviceJSONResponse.filter(r => r['_ObjectType_'] === 'SP.Taxonomy.TermCollection');
+              const termStoreResult: ITerms[] = serviceJSONResponse.filter((r: { [x: string]: string; }) => r['_ObjectType_'] === 'SP.Taxonomy.TermCollection');
               if (termStoreResult.length > 0) {
                 // Retrieve all terms
 
@@ -302,15 +302,15 @@ export default class SPTermStorePickerService {
    * @param b term 2
    */
   private _sortTerms(a: ITerm, b: ITerm) {
-    if(a.CustomSortOrderIndex === -1){
-      if (a.PathOfTerm < b.PathOfTerm) {
+    if (a.CustomSortOrderIndex === -1) {
+      if (a.PathOfTerm.toLowerCase() < b.PathOfTerm.toLowerCase()) {
         return -1;
       }
-      if (a.PathOfTerm > b.PathOfTerm) {
+      if (a.PathOfTerm.toLowerCase() > b.PathOfTerm.toLowerCase()) {
         return 1;
       }
       return 0;
-    }else{
+    } else {
       if (a.CustomSortOrderIndex < b.CustomSortOrderIndex) {
         return -1;
       }
@@ -319,7 +319,6 @@ export default class SPTermStorePickerService {
       }
       return 0;
     }
-
   }
 
   /**
