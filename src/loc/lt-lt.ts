74d06adc4d537a0de4a552bf4b4f9376ad9ab0bc
Tautvydas Duda <ltdu@users.noreply.github.com>
2019-04-27 20:51:10 +0300

    reorder based on mystrings.d.ts; normalise quotation

--- src/loc/lt-lt.ts
+++ src/loc/lt-lt.ts
@@ -2,86 +2,61 @@ declare var define: any;
 
 define([], () => {
   return {
-    'SiteBreadcrumbLabel': 'Svetainės navigacijos grandinė',
-
-    'ListViewGroupEmptyLabel': 'Nenurodyta',
-
-    'WebPartTitlePlaceholder': 'Puslapio dalies antraštė',
-    'WebPartTitleLabel': 'Pridėti antraštę',
-
-    "DateTime": {
-      "L_RelativeDateTime_AFewSecondsFuture": "po kelių sekundžių",
-      "L_RelativeDateTime_AFewSeconds": "prieš kelias sekundes",
-      "L_RelativeDateTime_AboutAMinuteFuture": "maždaug po minutės",
-      "L_RelativeDateTime_AboutAMinute": "beveik prieš minutę",
-      "L_RelativeDateTime_XMinutesFuture": "po {0} min.",
-      "L_RelativeDateTime_XMinutes": "prieš {0} min.",
-      "L_RelativeDateTime_XMinutesFutureIntervals": "0-",
-      "L_RelativeDateTime_XMinutesIntervals": "0-",
-      "L_RelativeDateTime_AboutAnHourFuture": "maždaug po valandos",
-      "L_RelativeDateTime_AboutAnHour": "beveik prieš valandą",
-      "L_RelativeDateTime_Tomorrow": "rytoj",
-      "L_RelativeDateTime_Yesterday": "vakar",
-      "L_RelativeDateTime_TomorrowAndTime": "rytoj, {0}",
-      "L_RelativeDateTime_YesterdayAndTime": "vakar, {0}",
-      "L_RelativeDateTime_XHoursFuture": "po {0} val.",
-      "L_RelativeDateTime_XHours": "prieš {0} val.",
-      "L_RelativeDateTime_XHoursFutureIntervals": "0-",
-      "L_RelativeDateTime_XHoursIntervals": "0-",
-      "L_RelativeDateTime_DayAndTime": "{0}, {1}",
-      "L_RelativeDateTime_XDaysFuture": "po {0} d.",
-      "L_RelativeDateTime_XDays": "prieš {0} d.",
-      "L_RelativeDateTime_XDaysFutureIntervals": "0-",
-      "L_RelativeDateTime_XDaysIntervals": "0-",
-      "L_RelativeDateTime_Today": "šiandien"
-    },
-    "SendEmailTo": "Siųsti el. laišką {0}",
-    "StartChatWith": "Pradėti pokalbį su {0}",
-    "Contact": "Kontaktas",
-    "UpdateProfile": "Atnaujinti profilį",
-
-    "TaxonomyPickerNoTerms": "Terminų rinkinys tuščias",
-    "TaxonomyPickerExpandTitle": "Išskleisti terminų rinkinį",
-    "TaxonomyPickerMenuTermSet": "Terminų rinkinio meniu",
-    "TaxonomyPickerInLabel": "",
-    "TaxonomyPickerTermSetLabel": "Terminų rinkinys",
-
-    peoplePickerComponentTooltipMessage: "Pasirinkite naudotojus",
-    peoplePickerComponentErrorMessage: "Privalomas laukas",
-    peoplePickerSuggestionsHeaderText: 'Pasiūlymai',
-    peoplePickerLoadingText: 'Įkeliama',
-    PeoplePickerSearchText: 'Ieškoma naudotojų',
     PeoplePickerGroupNotFound: "Grupės nepavyko rasti.",
-
-    genericNoResultsFoundText: 'Rezultatų nėra',
-
-    ListItemPickerSelectValue: 'Pasirinkite reikšmę',
-    ListItemAttachmentsActionDeleteIconTitle: 'Naikinti',
-    ListItemAttachmentsactionDeleteTitle: 'Naikinti',
-    ListItemAttachmentsfileDeletedMsg: 'Failas „{0}“ panaikintas',
-    ListItemAttachmentsfileDeleteError: 'Klaida naikinant failą „{0}“: {1}',
-    ListItemAttachmentserrorLoadAttachments: 'Nepavyko įkelti elemento priedų infomacijos: {0}',
-    ListItemAttachmentsconfirmDelete: 'Ar tikrai norite priedą „{0}“ siųsti į svetainės šiukšlinę?',
-    ListItemAttachmentsdialogTitle: 'Elemento priedai',
-    ListItemAttachmentsdialogOKbuttonLabel: 'Gerai',
-    ListItemAttachmentsdialogCancelButtonLabel: 'Atšaukti',
-    ListItemAttachmentsdialogOKbuttonLabelOnDelete: 'Naikinti',
-    ListItemAttachmentsuploadAttachmentDialogTitle: 'Įkelti priedą',
-    ListItemAttachmentsuploadAttachmentButtonLabel: 'Įkelti priedą',
-    ListItemAttachmentsuploadAttachmentErrorMsg: 'Failo „{0}“ įkelti nepavyko: {1}',
-    ListItemAttachmentsCommandBarAddAttachmentLabel: 'Įkelti priedą',
-    ListItemAttachmentsloadingMessage: 'Failas siunčiamas...',
-    ListItemAttachmentslPlaceHolderIconText: 'Elemento priedai',
-    ListItemAttachmentslPlaceHolderDescription: 'Įkelkite elemento priedų', 
-    ListItemAttachmentslPlaceHolderButtonLabel: 'Pridėti',
-
-    mapsErrorMessage: 'Įkeliant žemėlapį įvyko klaida',
-    mapsLoadingText: 'Įkeliama',
-    mapsSearchButtonText: 'Paieška',
-    mapsTitlePrefix: 'Žemėlapis',
-
     ListViewFilterLabel: "Filtruoti",
 
+    PeoplePickerSearchText: "Ieškoma naudotojų",
+    peoplePickerComponentTooltipMessage: "Pasirinkite naudotojus",
+    peoplePickerComponentErrorMessage: "Privalomas laukas",
+    peoplePickerSuggestionsHeaderText: "Pasiūlymai",
+    genericNoResultsFoundText: "Rezultatų nėra",
+    peoplePickerLoadingText: "Įkeliama",
+
+    SiteBreadcrumbLabel: "Svetainės navigacijos grandinė",
+    ListViewGroupEmptyLabel: "Nenurodyta",
+    WebPartTitlePlaceholder: "Puslapio dalies antraštė",
+    WebPartTitleLabel: "Pridėti antraštę",
+    // DateTime: IDateTimeStrings;
+    SendEmailTo: "Siųsti el. laišką {0}",
+    StartChatWith: "Pradėti pokalbį su {0}",
+    Contact: "Kontaktas",
+    UpdateProfile: "Atnaujinti profilį",
+
+    // Taxonomy picker
+    TaxonomyPickerNoTerms: "Terminų rinkinys tuščias",
+    TaxonomyPickerExpandTitle: "Išskleisti terminų rinkinį",
+    TaxonomyPickerMenuTermSet: "Terminų rinkinio meniu",
+    TaxonomyPickerInLabel: "",
+    TaxonomyPickerTermSetLabel: "Terminų rinkinys",
+
+    ListItemPickerSelectValue: "Pasirinkite reikšmę",
+
+    ListItemAttachmentsActionDeleteIconTitle: "Naikinti",
+    ListItemAttachmentsactionDeleteTitle: "Naikinti",
+    ListItemAttachmentsfileDeletedMsg: "Failas „{0}“ panaikintas",
+    ListItemAttachmentsfileDeleteError: "Klaida naikinant failą „{0}“: {1}",
+    ListItemAttachmentserrorLoadAttachments: "Nepavyko įkelti elemento priedų infomacijos: {0}",
+    ListItemAttachmentsconfirmDelete: "Ar tikrai norite priedą „{0}“ siųsti į svetainės šiukšlinę?",
+    ListItemAttachmentsdialogTitle: "Elemento priedai",
+    ListItemAttachmentsdialogOKbuttonLabel: "Gerai",
+    ListItemAttachmentsdialogCancelButtonLabel: "Atšaukti",
+    ListItemAttachmentsdialogOKbuttonLabelOnDelete: "Naikinti",
+    ListItemAttachmentsuploadAttachmentDialogTitle: "Įkelti priedą",
+    ListItemAttachmentsuploadAttachmentButtonLabel: "Įkelti priedą",
+    ListItemAttachmentsuploadAttachmentErrorMsg: "Failo „{0}“ įkelti nepavyko: {1}",
+    ListItemAttachmentsCommandBarAddAttachmentLabel: "Įkelti priedą",
+    ListItemAttachmentsloadingMessage: "Failas siunčiamas...",
+    ListItemAttachmentslPlaceHolderIconText: "Elemento priedai",
+    ListItemAttachmentslPlaceHolderDescription: "Įkelkite elemento priedų",
+    ListItemAttachmentslPlaceHolderButtonLabel: "Pridėti",
+
+    //Maps
+    mapsErrorMessage: "Įkeliant žemėlapį įvyko klaida",
+    mapsLoadingText: "Įkeliama",
+    mapsSearchButtonText: "Paieška",
+    mapsTitlePrefix: "Žemėlapis",
+
+    // RichText
     HeaderNormalText: "Paprastas tekstas",
     HeaderH2: "1 antraštė",
     HeaderH3: "2 antraštė",
@@ -164,6 +139,7 @@ define([], () => {
     StandardColorDarkblue: "Tamsiai mėlyna",
     StandardColorPurple: "Violetinė",
 
+    // DateTime picker
     DatePickerMonthLongJanuary: "sausis",
     DatePickerMonthShortJanuary: "saus.",
     DatePickerMonthLongFebruary: "vasaris",
@@ -209,6 +185,33 @@ define([], () => {
     DateTimePickerDate: "Data",
     DateTimePickerTime: "Laikas",
     DateTimePickerTimeSeparator: ":",
-    DateTimePickerHourValueInvalid: "Netinkama laiko reikšmė"
+    DateTimePickerHourValueInvalid: "Netinkama laiko reikšmė",
+
+    DateTime: {
+      L_RelativeDateTime_AFewSecondsFuture: "po kelių sekundžių",
+      L_RelativeDateTime_AFewSeconds: "prieš kelias sekundes",
+      L_RelativeDateTime_AboutAMinuteFuture: "maždaug po minutės",
+      L_RelativeDateTime_AboutAMinute: "beveik prieš minutę",
+      L_RelativeDateTime_XMinutesFuture: "po {0} min.",
+      L_RelativeDateTime_XMinutes: "prieš {0} min.",
+      L_RelativeDateTime_XMinutesFutureIntervals: "0-",
+      L_RelativeDateTime_XMinutesIntervals: "0-",
+      L_RelativeDateTime_AboutAnHourFuture: "maždaug po valandos",
+      L_RelativeDateTime_AboutAnHour: "beveik prieš valandą",
+      L_RelativeDateTime_Tomorrow: "rytoj",
+      L_RelativeDateTime_Yesterday: "vakar",
+      L_RelativeDateTime_TomorrowAndTime: "rytoj, {0}",
+      L_RelativeDateTime_YesterdayAndTime: "vakar, {0}",
+      L_RelativeDateTime_XHoursFuture: "po {0} val.",
+      L_RelativeDateTime_XHours: "prieš {0} val.",
+      L_RelativeDateTime_XHoursFutureIntervals: "0-",
+      L_RelativeDateTime_XHoursIntervals: "0-",
+      L_RelativeDateTime_DayAndTime: "{0}, {1}",
+      L_RelativeDateTime_XDaysFuture: "po {0} d.",
+      L_RelativeDateTime_XDays: "prieš {0} d.",
+      L_RelativeDateTime_XDaysFutureIntervals: "0-",
+      L_RelativeDateTime_XDaysIntervals: "0-",
+      L_RelativeDateTime_Today: "šiandien"
+    }
   };
 });
