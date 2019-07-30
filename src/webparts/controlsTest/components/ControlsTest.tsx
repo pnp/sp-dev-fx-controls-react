d2cb83d717b439897cffce8320e083887156a18a
Hugo Bernier <hugoabernier@live.ca>
2019-07-29 21:46:28 -0400

    Update ControlsTest.tsx

--- src/webparts/controlsTest/components/ControlsTest.tsx
+++ src/webparts/controlsTest/components/ControlsTest.tsx
@@ -55,42 +55,42 @@ import { ImageFit } from 'office-ui-fabric-react/lib/Image';
 /**
  * The sample data below was randomly generated (except for the title). It is used by the grid layout
  */
-const sampleGridData: any[] = [{
-  thumbnail: "https://pixabay.com/get/57e9dd474952a414f1dc8460825668204022dfe05555754d742e7bd6/hot-air-balloons-1984308_640.jpg",
-  title: "Adventures in SPFx",
-  name: "Perry Losselyong",
-  profileImageSrc: "https://robohash.org/blanditiisadlabore.png?size=50x50&set=set1",
-  location: "SharePoint",
-  activity: "3/13/2019"
-}, {
-  thumbnail: "https://pixabay.com/get/55e8d5474a52ad14f1dc8460825668204022dfe05555754d742d79d0/autumn-3804001_640.jpg",
-  title: "The Wild, Untold Story of SharePoint!",
-  name: "Ebonee Gallyhaock",
-  profileImageSrc: "https://robohash.org/delectusetcorporis.bmp?size=50x50&set=set1",
-  location: "SharePoint",
-  activity: "6/29/2019"
-}, {
-  thumbnail: "https://pixabay.com/get/57e8dd454c50ac14f1dc8460825668204022dfe05555754d742c72d7/log-cabin-1886620_640.jpg",
-  title: "Low Code Solutions: PowerApps",
-  name: "Seward Keith",
-  profileImageSrc: "https://robohash.org/asperioresautquasi.jpg?size=50x50&set=set1",
-  location: "PowerApps",
-  activity: "12/31/2018"
-}, {
-  thumbnail: "https://pixabay.com/get/55e3d445495aa514f1dc8460825668204022dfe05555754d742b7dd5/portrait-3316389_640.jpg",
-  title: "Not Your Grandpa's SharePoint",
-  name: "Sharona Selkirk",
-  profileImageSrc: "https://robohash.org/velnammolestiae.png?size=50x50&set=set1",
-  location: "SharePoint",
-  activity: "11/20/2018"
-}, {
-  thumbnail: "https://pixabay.com/get/57e6dd474352ae14f1dc8460825668204022dfe05555754d742a7ed1/faucet-1684902_640.jpg",
-  title: "Get with the Flow",
-  name: "Boyce Batstone",
-  profileImageSrc: "https://robohash.org/nulladistinctiomollitia.jpg?size=50x50&set=set1",
-  location: "Flow",
-  activity: "5/26/2019"
-}];
+const sampleGridData: any[] =  [{
+        thumbnail: "https://lorempixel.com/400/200/technics/1/",
+        title: "Adventures in SPFx",
+        name: "Perry Losselyong",
+        profileImageSrc: "https://robohash.org/blanditiisadlabore.png?size=50x50&set=set1",
+        location: "SharePoint",
+        activity: "3/13/2019"
+      }, {
+        thumbnail: "https://lorempixel.com/400/200/technics/2",
+        title: "The Wild, Untold Story of SharePoint!",
+        name: "Ebonee Gallyhaock",
+        profileImageSrc: "https://robohash.org/delectusetcorporis.bmp?size=50x50&set=set1",
+        location: "SharePoint",
+        activity: "6/29/2019"
+      }, {
+        thumbnail: "https://lorempixel.com/400/200/technics/3",
+        title: "Low Code Solutions: PowerApps",
+        name: "Seward Keith",
+        profileImageSrc: "https://robohash.org/asperioresautquasi.jpg?size=50x50&set=set1",
+        location: "PowerApps",
+        activity: "12/31/2018"
+      }, {
+        thumbnail: "https://lorempixel.com/400/200/technics/4",
+        title: "Not Your Grandpa's SharePoint",
+        name: "Sharona Selkirk",
+        profileImageSrc: "https://robohash.org/velnammolestiae.png?size=50x50&set=set1",
+        location: "SharePoint",
+        activity: "11/20/2018"
+      }, {
+        thumbnail: "https://lorempixel.com/400/200/technics/5/",
+        title: "Get with the Flow",
+        name: "Boyce Batstone",
+        profileImageSrc: "https://robohash.org/nulladistinctiomollitia.jpg?size=50x50&set=set1",
+        location: "Flow",
+        activity: "5/26/2019"
+      }];
 
 /**
  * Component that can be used to test out the React controls from this project
