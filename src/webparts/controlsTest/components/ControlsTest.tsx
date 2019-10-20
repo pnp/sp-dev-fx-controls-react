959035b2dbbec2f90c85d8cb12a0f95d3c9cf672
João Mendes <joao.j.mendes@outlook.com>
2019-10-20 16:13:43 +0100

    Update ControlsTest.tsx

--- src/webparts/controlsTest/components/ControlsTest.tsx
+++ src/webparts/controlsTest/components/ControlsTest.tsx
@@ -693,13 +693,15 @@ export default class ControlsTest extends React.Component<IControlsTestProps, IC
               </div>
 
               <div className="ms-font-m">Field picker list data tester:
+
                 <ListItemPicker listId={'76a8231b-35b6-4703-b1f4-5d03d3dfb1ca'}
-                  columnInternalName="DescricaoItem"
-                  keyColumnInternalName="Empresa"
-                  filterList={"TipodePedido eq 'Ambos'"}
+                  columnInternalName="Title"
+                  keyColumnInternalName="Id"
+                  filterList={"Title eq 'SPFx'"}
                   itemLimit={5}
                   context={this.props.context}
                   onSelectedItem={this.listItemPickerDataSelected} />
+                  
               </div>
 
               <div className="ms-font-m">Services tester:
