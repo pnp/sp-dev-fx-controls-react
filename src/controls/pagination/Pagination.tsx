6917c3eaf9c6569d5060aaa0649848be4a8be048
Gautam Sheth <gautamdsheth@outlook.com>
2020-05-01 12:25:06 +0530

    Use the explicit path - minor optimization

--- src/controls/pagination/Pagination.tsx
+++ src/controls/pagination/Pagination.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { PrimaryButton, DefaultButton } from 'office-ui-fabric-react';
+import { PrimaryButton, DefaultButton } from 'office-ui-fabric-react/lib/Button';
 import styles from "./Pagination.module.scss";
 
 export interface IPaginationProps {
