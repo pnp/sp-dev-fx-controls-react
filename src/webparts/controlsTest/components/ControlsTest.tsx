f433e990479cc6affbd07c15ca46d9071319229e
Alex Terentiev <aleksei.terentiev@gmail.com>
2020-12-17 16:29:56 -0800

    support for custom rendering of Placeholder's iconText and description

--- src/webparts/controlsTest/components/ControlsTest.tsx
+++ src/webparts/controlsTest/components/ControlsTest.tsx
@@ -885,7 +885,7 @@ export default class ControlsTest extends React.Component<IControlsTestProps, IC
 
         <Placeholder iconName='Edit'
           iconText='Configure your web part'
-          description='Please configure the web part.'
+          description={defaultClassNames => <span className={defaultStyle}>Please configure the web part.</span>}
           buttonLabel='Configure'
           hideButton={this.props.displayMode === DisplayMode.Read}
           onConfigure={this._onConfigure} />
