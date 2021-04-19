3c618584d7b900d9a8dc6b4f46e85670ed113c8d
João Mendes <joao.j.mendes@outlook.com>
2021-04-19 11:15:04 +0100

    commit changes

--- release/assets/ControlsTestWebPartStrings_fr-fr_c025af983cbb7ad16483a4ef1b592195.js
+++
@@ -1,7 +0,0 @@
-define([], function() {
-  return {
-    'PropertyPaneDescription': 'Description',
-    'BasicGroupName': 'Nom du groupe',
-    'DescriptionFieldLabel': 'Champ Description'
-  }
-});
