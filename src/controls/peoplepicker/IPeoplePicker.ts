2fca874cb53650952584f6a92ada7e5519755d0d
Patrik Hellgren <patrik.hellgren@sherpas.se>
2021-05-27 16:36:07 +0200

    Added groupId property to People Picker

--- src/controls/peoplepicker/IPeoplePicker.ts
+++ src/controls/peoplepicker/IPeoplePicker.ts
@@ -30,6 +30,10 @@ export interface IPeoplePickerProps {
    */
   groupName?: string;
   /**
+   * Id of SharePoint Group
+   */
+   groupId?: number;
+   /**
    * Maximum number of suggestions to show in the full suggestion list. (default: 5)
    */
   suggestionsLimit?: number;
