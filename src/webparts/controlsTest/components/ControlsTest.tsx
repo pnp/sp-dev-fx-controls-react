728ea885a719b8943f6c6e46fc2261acc7e143f1
guido-zambarda <guido@piasys.it>
2023-07-11 18:23:12 +0200

    Restored the default DynamicForm listId

--- src/webparts/controlsTest/components/ControlsTest.tsx
+++ src/webparts/controlsTest/components/ControlsTest.tsx
@@ -1090,12 +1090,12 @@ export default class ControlsTest extends React.Component<IControlsTestProps, IC
         <div id="DynamicFormDiv" className={styles.container} hidden={!isDynamicFormDivVisible}>
           <div className="ms-font-m">
             {/* Change the list Id and list item id before you start to test this control */}
-            <DynamicForm context={this.props.context} listId={"F2AECDE9-2D98-49F2-99ED-B0A3EB107141"} onCancelled={() => { console.log('Cancelled'); }} onSubmitted={async (listItem) => { let itemdata = await listItem.get(); console.log(itemdata["ID"]); }}></DynamicForm>
+            <DynamicForm context={this.props.context} listId={"8d26295d-c532-47c6-a2c5-d6e79c2ef523"} onCancelled={() => { console.log('Cancelled'); }} onSubmitted={async (listItem) => { let itemdata = await listItem.get(); console.log(itemdata["ID"]); }}></DynamicForm>
           </div>
           <div className="ms-font-m">
             {/* Change the list Id and list item id before you start to test this control */}
             {/* This DynamicForm display a dialog message when validation fails */}
-            <DynamicForm context={this.props.context} listId={"F2AECDE9-2D98-49F2-99ED-B0A3EB107141"} onCancelled={() => { console.log('Cancelled'); }} onSubmitted={async (listItem) => { let itemdata = await listItem.get(); console.log(itemdata["ID"]); }} validationErrorDialogProps={{ showDialogOnValidationError: true }}></DynamicForm>
+            <DynamicForm context={this.props.context} listId={"8d26295d-c532-47c6-a2c5-d6e79c2ef523"} onCancelled={() => { console.log('Cancelled'); }} onSubmitted={async (listItem) => { let itemdata = await listItem.get(); console.log(itemdata["ID"]); }} validationErrorDialogProps={{ showDialogOnValidationError: true }}></DynamicForm>
           </div>
         </div>
         <div id="TeamsDiv" className={styles.container} hidden={!isTeamsDivVisible}>
