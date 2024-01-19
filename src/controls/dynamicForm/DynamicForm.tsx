e2d39839729450ed5fad020378b310bad6cf8603
Michaël Maillot <battosaimykle@gmail.com>
2024-01-19 23:00:50 +0100

    Fixing multi taxonomy field (loading + saving existing item)

--- src/controls/dynamicForm/DynamicForm.tsx
+++ src/controls/dynamicForm/DynamicForm.tsx
@@ -415,7 +415,7 @@ export class DynamicForm extends React.Component<
         if (field.newValue !== null && field.newValue !== undefined) {
 
           let value = field.newValue;
-          if (["Lookup", "LookupMulti", "User", "UserMulti"].indexOf(fieldType) < 0) {
+          if (["Lookup", "LookupMulti", "User", "UserMulti", "TaxonomyFieldTypeMulti"].indexOf(fieldType) < 0) {
             objects[columnInternalName] = value;
           }
 
@@ -1200,7 +1200,7 @@ export class DynamicForm extends React.Component<
               });
             });
 
-            defaultValue = selectedTags;
+            value = selectedTags;
           } else {
             if (defaultValue && defaultValue !== "") {
               defaultValue.split(/#|;/).forEach((element) => {
