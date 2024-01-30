9277bd2249845c27e64f3cb77d91d4afcdfd455c
matteo <>
2024-01-31 00:08:22 +0100

    calendar restricted dates

--- src/webparts/controlsTest/components/ControlsTest.tsx
+++ src/webparts/controlsTest/components/ControlsTest.tsx
@@ -1449,6 +1449,8 @@ export default class ControlsTest extends React.Component<IControlsTestProps, IC
           />
 
           <DateTimePicker label="DateTime Picker (disabled)" disabled={true} />
+
+          <DateTimePicker label="DateTime Picker (restricted dates)" isMonthPickerVisible={false} showSeconds={false} onChange={(value) => console.log("DateTimePicker value:", value)} placeholder="Pick a date" restrictedDates={[new Date(2024,1,15), new Date(2024,1,16), new Date(2024,1,17)]}/>
         </div>
         <div id="RichTextDiv" className={styles.container} hidden={!isRichTextDivVisible}>
           {/* <RichText isEditMode={this.props.displayMode === DisplayMode.Edit} onChange={value => { this.richTextValue = value; return value; }} /> */}
