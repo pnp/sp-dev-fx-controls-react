cd9d3b80eb384f59d2fba9a938ebe4f150474cdf
Michaël Maillot <battosaimykle@gmail.com>
2024-09-23 07:39:05 +0200

    Removing locale references for SPFx test components, to allow local running of the Controls in any language (#1882)

--- src/webparts/controlsTest/loc/en-us.js
+++
@@ -1,9 +0,0 @@
-define([], function() {
-  return {
-    'PropertyPaneDescription': 'Change settings below',
-    'BasicGroupName': 'Group Name',
-    'ControlsGroupName': 'Controls',
-    'ControlSettingsGroupName': 'Control Settings',
-    'DescriptionFieldLabel': 'Description Field'
-  }
-});
