ca199830b4f043bd6ce6dcbc700fb494e9a88385
Alex Terentiev <aleksei.terentiev@gmail.com>
2024-09-23 19:43:59 -0400

    more fixes

--- apps/controls-tests/gulpfile.js
+++ apps/controls-tests/gulpfile.js
@@ -13,4 +13,9 @@ build.rig.getTasks = function () {
   return result;
 };
 
+/* fast-serve */
+const { addFastServe } = require("spfx-fast-serve-helpers");
+addFastServe(build);
+/* end of fast-serve */
+
 build.initialize(require('gulp'));
