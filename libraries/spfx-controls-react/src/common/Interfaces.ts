a0b74f35b2cb1c622c54741cfd988f640aaba258
Alex Terentiev <aleksei.terentiev@gmail.com>
2025-02-19 21:06:22 -0500

    it is building

--- libraries/spfx-controls-react/src/common/Interfaces.ts
+++ libraries/spfx-controls-react/src/common/Interfaces.ts
@@ -1,6 +1,6 @@
 import { SPHttpClient } from '@microsoft/sp-http';
 import { PageContext, SPField } from '@microsoft/sp-page-context';
-import { ListViewAccessor } from "@microsoft/sp-listview-extensibility";
+import { ListViewAccessor } from '@microsoft/sp-listview-extensibility';
 import { ISPField } from './SPEntities';
 import { INavNodeInfo } from '@pnp/sp/navigation/types';
 
@@ -9,42 +9,42 @@ import { INavNodeInfo } from '@pnp/sp/navigation/types';
  * Can be used in different types of customizers
  */
 export interface IContext {
-    spHttpClient: SPHttpClient;
-    pageContext: PageContext;
-    listView?: ListViewAccessor | undefined;
-    field?: SPField | undefined;
+  spHttpClient: SPHttpClient;
+  pageContext: PageContext;
+  listView?: ListViewAccessor | undefined;
+  field?: SPField | undefined;
 }
 
 /**
  * Interface that represents dictionary of fields
  */
 export interface IFields {
-    [id: string]: ISPField;
+  [id: string]: ISPField;
 }
 
 /**
  * Parent of all props interfaces that needs context
  */
 export interface IProps {
-    context: IContext;
+  context: IContext;
 }
 
 export interface IHubSiteData {
-    headerEmphasis: string;
-    hideNameInNavigation: boolean;
-    isNavAudienceTargeted: boolean;
-    isSameTenantInstance: boolean;
-    logoFileHash: number;
-    logoUrl: string;
-    megaMenuEnabled: boolean;
-    name: string;
-    navigation: INavNodeInfo[];
-    parentHubSiteId: string;
-    relatedHubSiteIds: string[];
-    requiresJoinApproval: boolean;
-    siteDesignId: string;
-    tenantInstanceId: string;
-    themeKey: string;
-    url: string;
-    usesMetadataNavigation: boolean;
+  headerEmphasis: string;
+  hideNameInNavigation: boolean;
+  isNavAudienceTargeted: boolean;
+  isSameTenantInstance: boolean;
+  logoFileHash: number;
+  logoUrl: string;
+  megaMenuEnabled: boolean;
+  name: string;
+  navigation: INavNodeInfo[];
+  parentHubSiteId: string;
+  relatedHubSiteIds: string[];
+  requiresJoinApproval: boolean;
+  siteDesignId: string;
+  tenantInstanceId: string;
+  themeKey: string;
+  url: string;
+  usesMetadataNavigation: boolean;
 }
