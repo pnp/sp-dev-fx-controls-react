1cbefcd8f383903eb1f9935adde830d9b5a612e9
Patrik Hellgren <patrik.hellgren@sherpas.se>
2025-05-05 22:00:34 +0200

    Add fallback user search for guests

--- src/controls/listItemComments/components/AddComment/AddComment.tsx
+++ src/controls/listItemComments/components/AddComment/AddComment.tsx
@@ -11,17 +11,33 @@ import { IconButton } from "@fluentui/react/lib/Button";
 import { Text} from "@fluentui/react/lib/Text";
 import { ECommentAction } from "../../common/ECommentAction";
 import { IAddCommentPayload } from "../../models/IAddCommentPayload";
-import { useMsGraphAPI } from "../..";
+import { AppContext, useMsGraphAPI } from "../..";
+import SPPeopleSearchService from "../../../../services/PeopleSearchService";
+import { MSGraphClientFactory, SPHttpClient } from "@microsoft/sp-http";
+import { PageContext } from "@microsoft/sp-page-context";
+import { PrincipalType } from "../../../peoplepicker";
 
 export interface IAddCommentProps {}
 
 export const AddComment: React.FunctionComponent<IAddCommentProps> = (props: IAddCommentProps) => {
   const [commentText, setCommentText] = useState<string>("");
+  const [disableCallingGraph, setDisableCallingGraph] = useState<boolean>(false);
   const { getUsers, getSuggestions } = useMsGraphAPI();
   const { reactMentionStyles, mentionsClasses, componentClasses } = useAddCommentStyles();
   const [singleLine, setSingleLine] = useState<boolean>(true);
   const { setlistItemCommentsState } = useContext(ListItemCommentsStateContext);
   const _addCommentText = useRef<IAddCommentPayload>({ mentions: [], text: "" });
+  const { serviceScope } = useContext(AppContext);
+  let _msGraphClientFactory: MSGraphClientFactory = undefined;
+  let _sPHttpClient: SPHttpClient = undefined;
+  let _pageContext: PageContext = undefined;
+  let _peopleSearchService: SPPeopleSearchService = undefined;
+  serviceScope.whenFinished(async () => {
+    _msGraphClientFactory = serviceScope.consume(MSGraphClientFactory.serviceKey);
+    _sPHttpClient = serviceScope.consume(SPHttpClient.serviceKey);
+    _pageContext = serviceScope.consume(PageContext.serviceKey);
+    _peopleSearchService = new SPPeopleSearchService({absoluteUrl: _pageContext.web.absoluteUrl, msGraphClientFactory: _msGraphClientFactory, spHttpClient: _sPHttpClient}, false);
+  });
 
   const sugestionsContainer = useRef<HTMLDivElement>();
   let _reactMentionStyles = reactMentionStyles;
@@ -54,17 +70,47 @@ export const AddComment: React.FunctionComponent<IAddCommentProps> = (props: IAd
   }, []);
 
   const _searchData = (search: string, callback: (users: SuggestionDataItem[]) => void): void => {
+    const _searchPeople = (): void => {
+      _peopleSearchService.searchPeople(search, 5, [PrincipalType.User])
+      .then((res) => res.map((user) => ({ display: user.text, id: user.secondaryText })))
+      .then(callback)
+      .catch(() => { /* no-op; */ });
+    };
+
+    if (disableCallingGraph) {
+      _searchPeople();
+      return;
+    }
+
     // Try to get sugested users when user type '@'
     if (!search) {
       getSuggestions()
         .then((res) => res.users.map((user) => ({ display: user.displayName, id: user.mail })))
         .then(callback)
-        .catch(() => { /* no-op; */ });
+        .catch((error) => {
+          switch (error.statusCode) {
+            case 403:
+            case 404:
+              // If the user is not allowed to call graph API (e.g. guest users), we need to use the People Search API
+              setDisableCallingGraph(true);
+              break;
+            default:
+          }
+        });
     } else {
       getUsers(search)
         .then((res) => res.users.map((user) => ({ display: user.displayName, id: user.mail })))
         .then(callback)
-        .catch(() => { /* no-op; */ });
+        .catch((error) => {
+          switch (error.statusCode) {
+            case 403:
+              // If the user is not allowed to call graph API (e.g. guest users), we need to use the People Search API
+              setDisableCallingGraph(true);
+              _searchPeople();
+              break;
+            default:
+          }
+        });
     }
   };
 
